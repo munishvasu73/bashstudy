849305ad5b6be6020e4a1796a2c8f19e2e775120
MunishVasudeva <munishvasudeva73@gmail.com>
2018-12-25 12:39:52 +0000

    backup1.sh

---
+++ backup1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author: Munish 
+#Date: 25/12/18
+#Purpose: backup for user
+
+user=$(whoami)
+input=/home/$user
+output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tgz
+
+tar -czf $output $input
+echo "Backup of $input completed! Details about output file:"
+ls -l $output
+
